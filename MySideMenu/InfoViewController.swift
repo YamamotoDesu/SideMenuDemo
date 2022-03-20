359574d4952f6c9f93c6205e9e6ce7460ea15896
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-03-20 19:56:08 +0900

    Initial Commit

---
+++ MySideMenu/InfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoViewController.swift
+//  MySideMenu
+//
+//  Created by 山本響 on 2022/03/20.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+}
---
+++ MySideMenu/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  MySideMenu
+//
+//  Created by 山本響 on 2022/03/20.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
